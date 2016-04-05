236b8506b84a16481447f044e95aa180dc750e85
Diego Laurentino <Diego Laurentino>
2016-04-05 15:07:48 -0300

    Created more layouts and java classes.

--- app/src/main/java/com/roakstudio/roomie/IntroActivity.java
+++ app/src/main/java/com/roakstudio/roomie/IntroActivity.java
@@ -3,20 +3,20 @@ package com.roakstudio.roomie;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class IntroActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_intro);
     }
 
     public void next() {
         Button buttonIntro = (Button) findViewById(R.id.btn_Login);
-        Intent intent = new Intent(getApplicationContext(), IntroActivity.class);
+        Intent intent = new Intent(getApplicationContext(), FormActivity.class);
         startActivity(intent);
     }
 }
