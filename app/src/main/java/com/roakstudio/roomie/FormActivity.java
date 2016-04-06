e81b231e70f9fe5caad89ad5efad78d4417f7f2b
Diego Laurentino <Diego Laurentino>
2016-04-06 13:35:13 -0300

    Made Spinners and Adapters for all form questions.

--- app/src/main/java/com/roakstudio/roomie/FormActivity.java
+++ app/src/main/java/com/roakstudio/roomie/FormActivity.java
@@ -12,8 +12,8 @@ import android.widget.Toast;
 
 public class FormActivity extends AppCompatActivity {
 
-    Spinner spinner;
-    ArrayAdapter<CharSequence> adapter;
+    Spinner spinner1, spinner2, spinner3, spinner4, spinner5, spinner6, spinner7, spinner8, spinner9, spinner10, spinner11, spinner12, spinner13, spinner14;
+    ArrayAdapter<CharSequence> adapter1, adapter2, adapter3, adapter4, adapter5, adapter6, adapter7, adapter8, adapter9, adapter10, adapter11, adapter12, adapter13, adapter14;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,11 +21,29 @@ public class FormActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form);
 
-        spinner = (Spinner) findViewById(R.id.spinner1);
-        adapter = ArrayAdapter.createFromResource(this, R.array.str_question_1_choices, android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(adapter);
-        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+        //Spinner and Adapter Question 1
+        spinner1 = (Spinner) findViewById(R.id.spinner1);
+        adapter1 = ArrayAdapter.createFromResource(this, R.array.str_question_1_choices, android.R.layout.simple_spinner_item);
+        adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner1.setAdapter(adapter1);
+        spinner1.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getBaseContext(), parent.getItemAtPosition(position) + " selected", Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        //Spinner and Adapter Question 2
+        spinner2 = (Spinner) findViewById(R.id.spinner2);
+        adapter2 = ArrayAdapter.createFromResource(this, R.array.str_question_2_choices, android.R.layout.simple_spinner_item);
+        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner2.setAdapter(adapter2);
+        spinner2.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                 Toast.makeText(getBaseContext(), parent.getItemAtPosition(position)+ " selected", Toast.LENGTH_LONG).show();
@@ -36,6 +54,211 @@ public class FormActivity extends AppCompatActivity {
 
             }
         });
+
+        //Spinner and Adapter Question 3
+        spinner3 = (Spinner) findViewById(R.id.spinner3);
+        adapter3 = ArrayAdapter.createFromResource(this, R.array.str_question_3_choices, android.R.layout.simple_spinner_item);
+        adapter3.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner3.setAdapter(adapter3);
+        spinner3.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getBaseContext(), parent.getItemAtPosition(position)+ " selected", Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        //Spinner and Adapter Question 4
+        spinner4 = (Spinner) findViewById(R.id.spinner4);
+        adapter4 = ArrayAdapter.createFromResource(this, R.array.str_question_4_choices, android.R.layout.simple_spinner_item);
+        adapter4.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner4.setAdapter(adapter4);
+        spinner4.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getBaseContext(), parent.getItemAtPosition(position)+ " selected", Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        //Spinner and Adapter Question 5
+        spinner5 = (Spinner) findViewById(R.id.spinner5);
+        adapter5 = ArrayAdapter.createFromResource(this, R.array.str_question_5_choices, android.R.layout.simple_spinner_item);
+        adapter5.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner5.setAdapter(adapter5);
+        spinner5.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getBaseContext(), parent.getItemAtPosition(position)+ " selected", Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        //Spinner and Adapter Question 6
+        spinner6 = (Spinner) findViewById(R.id.spinner6);
+        adapter6 = ArrayAdapter.createFromResource(this, R.array.str_question_6_choices, android.R.layout.simple_spinner_item);
+        adapter6.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner6.setAdapter(adapter6);
+        spinner6.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getBaseContext(), parent.getItemAtPosition(position)+ " selected", Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        //Spinner and Adapter Question 7
+        spinner7 = (Spinner) findViewById(R.id.spinner7);
+        adapter7 = ArrayAdapter.createFromResource(this, R.array.str_question_7_choices, android.R.layout.simple_spinner_item);
+        adapter7.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner7.setAdapter(adapter7);
+        spinner7.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getBaseContext(), parent.getItemAtPosition(position)+ " selected", Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        //Spinner and Adapter Question 8
+        spinner8 = (Spinner) findViewById(R.id.spinner8);
+        adapter8 = ArrayAdapter.createFromResource(this, R.array.str_question_8_choices, android.R.layout.simple_spinner_item);
+        adapter8.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner8.setAdapter(adapter8);
+        spinner8.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getBaseContext(), parent.getItemAtPosition(position)+ " selected", Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        //Spinner and Adapter Question 9
+        spinner9 = (Spinner) findViewById(R.id.spinner9);
+        adapter9 = ArrayAdapter.createFromResource(this, R.array.str_question_9_choices, android.R.layout.simple_spinner_item);
+        adapter9.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner9.setAdapter(adapter9);
+        spinner9.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getBaseContext(), parent.getItemAtPosition(position)+ " selected", Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        //Spinner and Adapter Question 10
+        spinner10 = (Spinner) findViewById(R.id.spinner10);
+        adapter10 = ArrayAdapter.createFromResource(this, R.array.str_question_10_choices, android.R.layout.simple_spinner_item);
+        adapter10.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner10.setAdapter(adapter10);
+        spinner10.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getBaseContext(), parent.getItemAtPosition(position)+ " selected", Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        //Spinner and Adapter Question 11
+        spinner11 = (Spinner) findViewById(R.id.spinner11);
+        adapter11 = ArrayAdapter.createFromResource(this, R.array.str_question_11_choices, android.R.layout.simple_spinner_item);
+        adapter11.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner11.setAdapter(adapter11);
+        spinner11.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getBaseContext(), parent.getItemAtPosition(position)+ " selected", Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        //Spinner and Adapter Question 12
+        spinner12 = (Spinner) findViewById(R.id.spinner12);
+        adapter12 = ArrayAdapter.createFromResource(this, R.array.str_question_12_choices, android.R.layout.simple_spinner_item);
+        adapter12.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner12.setAdapter(adapter12);
+        spinner12.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getBaseContext(), parent.getItemAtPosition(position)+ " selected", Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        //Spinner and Adapter Question 13
+        spinner13 = (Spinner) findViewById(R.id.spinner13);
+        adapter13 = ArrayAdapter.createFromResource(this, R.array.str_question_13_choices, android.R.layout.simple_spinner_item);
+        adapter13.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner13.setAdapter(adapter13);
+        spinner13.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getBaseContext(), parent.getItemAtPosition(position)+ " selected", Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        //Spinner and Adapter Question 14
+        spinner14 = (Spinner) findViewById(R.id.spinner14);
+        adapter14 = ArrayAdapter.createFromResource(this, R.array.str_question_14_choices, android.R.layout.simple_spinner_item);
+        adapter14.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner14.setAdapter(adapter14);
+        spinner14.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getBaseContext(), parent.getItemAtPosition(position)+ " selected", Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
     }
 
     public void sendMessage(View view) {
